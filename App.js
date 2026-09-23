f4ab9666519ab407ae21150f16482e5ad92f28c9
mycfoods <ezequiel89.c@gmail.com>
2026-09-22 23:17:45 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ const cartelera = [
         archivo: "media/fotos/Albóndigas con salsa y puré de papas_20260922_095614_0000.png",
         duracion: 5000
     },
+
     {
         tipo: "foto",
         archivo: "media/fotos/box_dia_de_la_madre_historia-4.jpg",
         duracion: 5000
     },
+
     {
         tipo: "foto",
         archivo: "media/fotos/file_00000000ee40820eb02e991bf44310ad.png",
@@ -16,43 +18,91 @@ const cartelera = [
     }
 ];
 
+
 const pantalla = document.getElementById("pantalla");
 
 let posicion = 0;
 
-function mostrarContenido() {
+
+function mostrarFoto(item) {
+
+    const slide = document.createElement("div");
+
+    slide.className = "slide";
+
+
+    const fondo = document.createElement("img");
+
+    fondo.className = "fondo";
+    fondo.src = item.archivo;
+    fondo.alt = "";
+
+
+    const imagen = document.createElement("img");
+
+    imagen.className = "imagen-principal";
+    imagen.src = item.archivo;
+    imagen.alt = "MYCFOODS";
+
+
+    const degradado = document.createElement("div");
+
+    degradado.className = "degradado";
+
+
+    imagen.onload = function () {
+
+        slide.appendChild(fondo);
+        slide.appendChild(imagen);
+        slide.appendChild(degradado);
+
+        pantalla.innerHTML = "";
+
+        pantalla.appendChild(slide);
+
+
+        setTimeout(() => {
+
+            siguiente();
+
+        }, item.duracion);
+
+    };
+
+
+    imagen.onerror = function () {
+
+        console.error(
+            "ERROR AL CARGAR:",
+            item.archivo
+        );
+
+        siguiente();
+
+    };
+
+}
+
+
+function mostrarActual() {
 
     const item = cartelera[posicion];
 
-    pantalla.innerHTML = "";
+    if (!item) {
+        posicion = 0;
+        mostrarActual();
+        return;
+    }
 
-    const slide = document.createElement("div");
-    slide.className = "slide";
 
     if (item.tipo === "foto") {
 
-        const imagen = document.createElement("img");
-
-        imagen.src = item.archivo;
-        imagen.alt = "MYCFOODS";
-
-        imagen.onload = function () {
-            slide.appendChild(imagen);
-            pantalla.appendChild(slide);
-
-            setTimeout(() => {
-                siguiente();
-            }, item.duracion);
-        };
-
-        imagen.onerror = function () {
-            console.error("No se pudo cargar:", item.archivo);
-            siguiente();
-        };
+        mostrarFoto(item);
 
     }
 
 }
+
 
 function siguiente() {
 
@@ -62,7 +112,9 @@ function siguiente() {
         posicion = 0;
     }
 
-    mostrarContenido();
+    mostrarActual();
+
 }
 
-mostrarContenido();
+
+mostrarActual();
